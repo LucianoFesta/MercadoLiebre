06ed9bbb94f7a88a7a994a9583838c871b3536f4
luchofesta <lucianoefesta@hotmail.com>
2022-08-10 20:48:21 -0300

    Subiendo el Proyecto a Keroku

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+
 const path = require('path');
 
 const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Servidor corriendo en el puerto ' + process.env.PORT);
 })
 
 app.get('/', (req, res) => {
